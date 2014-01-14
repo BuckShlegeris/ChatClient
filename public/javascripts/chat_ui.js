caca8175822b0aac68b22daa50798ee44cf9c1ba
Dan <dan.fan1988@gmail.com>
2014-01-13 16:36:30 -0800

    added nicks

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,24 +1,46 @@
 var getMessage = function () {
-  return $("#message").val()
+  return $("#message").val().replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
 }
 
 var sendMessage = function(message, socket) {
-  Chat.sendMessage(message, socket)
+  Chat.sendMessage(message, socket);
 }
 
-var addMessage = function(message) {
-  $(".messages").append("<p>"+message+"</p>");
+var addMessage = function(message, nick) {
+  $(".messages").append("<p>"+nick+": "+message+"</p>");
 }
 
 $(function() {
   var socket = io.connect();
   socket.on('message', function(data) {
     console.log(data);
-    debugger
-    addMessage(data.text);
+    addMessage(data.text, data.nick);
   });
 
+  // socket.on('message', function(data) {
+  //   console.log(data);
+  //   addMessage(data.text, data.nick);
+  // });
+
   $("#send-message").on('click', function() {
-    sendMessage(getMessage(), socket);
+    var message = getMessage();
+    if (message[0] === "/") {
+      var messageArray = message.split(" ");
+      var command = messageArray[0];
+      var args = messageArray.slice(1);
+      processCommand(command, args, socket);
+    } else {
+      sendMessage(message, socket);
+    }
   });
 });
+
+var sendNicknameChangeRequest = function (nickname, socket) {
+  socket.emit('nicknameChangeRequest', { 'nickname': nickname });
+}
+
+var processCommand = function (command, args, socket) {
+  if (command === "/nick") {
+    sendNicknameChangeRequest(args.join(" "), socket);
+  }
+};
